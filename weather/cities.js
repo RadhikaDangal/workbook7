5a9de5a8945cddb8fbcea822c58e457c57c68955
Radhika Dangal <rdangal@pgh.yearup.org>
2023-06-09 14:38:44 -0400

    final

---
+++ weather/cities.js
@@ -0,0 +1,6 @@
+let cities = [
+  { name: "Benbrook, TX", latitude: 32.6732, longitude: -97.4606 },
+  { name: "Pittsburgh, PA", latitude: 40.436072, longitude: -79.991315 },
+  { name: "Washington, DC", latitude: 38.896241, longitude: -77.036247 },
+  { name: "Brooklyn, NY", latitude: 40.697136, longitude: -73.997422 },
+];
